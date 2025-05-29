887cd0f9ec9321f1d0184d72328e3bc804379a84
Tetsuo Honda <t-honda@hondarer-soft.com>
2025-05-30 06:43:31 +0900

    Puppeteerの環境変数設定部を共通化

--- bin/mmdc-wrapper.sh
+++ bin/mmdc-wrapper.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Store the original Puppeteer executable path
-# Check if PUPPETEER_EXECUTABLE_PATH is undefined or set to an empty string
-if [[ ! -z "${PUPPETEER_EXECUTABLE_PATH}" ]]; then
-  export ORG_PUPPETEER_EXECUTABLE_PATH="$PUPPETEER_EXECUTABLE_PATH"
-fi
-
-# Set the PUPPETEER_EXECUTABLE_PATH to point to chrome-wrapper.sh in the same directory as this script
-export PUPPETEER_EXECUTABLE_PATH="$(dirname "$0")/chrome-wrapper.sh"
+# prepare to chrome-wrapper.sh
+. $(dirname "$0")/prepare_puppeteer_env.sh
 
 # Pass all arguments from shell script to mmdc
 $(dirname "$0")/node_modules/.bin/mmdc "$@"
